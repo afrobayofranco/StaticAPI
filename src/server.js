66e9b3409a5048fe851c651bae7d4ab86b74df73
afrobayofranco <afrobayofranco@fullsail.com>
2017-01-04 18:23:54 -0500

    Get and Post working

--- src/server.js
+++ src/server.js
@@ -1,32 +1,29 @@
 const express = require('express')
 const body_parser = require('body-parser')
-//const api = require('api.js')
+const api = require('api.js')
 const app = express();
 
 app.get('/api/v1/:url', function (req, res) {
-  var preUrl = "MyURLShortener";
-  var urlBreak = req.params.url; //Stores actual URL Input
+  var preUrl = "an.dy";
+  var oldUrl = req.params.url; //Stores actual URL Input
   var randomNum = Math.round(Math.random() * (9999 - 1000) + 1000); //randomizer
   var newURL = preUrl+"/"+randomNum;
 
   //New URL response
-  res.json({v1: {url: newURL}});
+  res.json({original_url: oldUrl, shortened_url: newURL});
+  //console.log(req.params.url);
 })
 
-app.use(body_parser.json());
+app.use(body_parser.json()); // support json encoded bodies
+app.use(body_parser.urlencoded({ extended: false })); // support encoded bodies
 app.post('/api/v1/url', function (req, res) {
-  res.send(req.body.url);
-  console.log(req.body.url);
-
-  /*
-  var preUrl = "MyURLShortener";
-  var urlBreak = req.params.url; //Stores actual URL Input
+  var preUrl = "an.dy";
+  var oldUrl = req.body.url; //Stores actual URL Input
   var randomNum = Math.round(Math.random() * (9999 - 1000) + 1000); //randomizer
   var newURL = preUrl+"/"+randomNum;
 
-  //New URL response
-  res.json({v1: {url: newURL}});
-  */
+  res.json({original_url: oldUrl, shortened_url: newURL});
+  //console.log(req.body.url);
 })
 
 app.listen(3000)
