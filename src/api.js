bac77e80cc9d5488d4e752a6ecba5140792584fc
afrobayofranco <afrobayofranco@fullsail.com>
2017-01-04 19:37:34 -0500

    Get and Post are woring on module api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,10 @@
-exports.api = () => {
-  console.log("I'm the url shortener.");
+exports.url_shortener = (oldUrl) => {
+
+    var preUrl = "an.dy";
+    var randomNum = Math.round(Math.random() * (9999 - 1000) + 1000); //randomizer
+    var newURL = preUrl+"/"+randomNum;
+
+    //New URL response
+    return {original_url: oldUrl, shortened_url: newURL};
+    //console.log(req.params.url);
 }
--- src/server.js
+++ src/server.js
@@ -1,28 +1,23 @@
 const express = require('express')
 const body_parser = require('body-parser')
-const api = require('api.js')
-const app = express();
+const api = require('./api.js')
+const app = express()
 
-app.get('/api/v1/:url', function (req, res) {
-  var preUrl = "an.dy";
+
+app.get('/api/v1/:url', (req, res) => {
   var oldUrl = req.params.url; //Stores actual URL Input
-  var randomNum = Math.round(Math.random() * (9999 - 1000) + 1000); //randomizer
-  var newURL = preUrl+"/"+randomNum;
 
-  //New URL response
-  res.json({original_url: oldUrl, shortened_url: newURL});
-  //console.log(req.params.url);
-})
+  res.json(api.url_shortener(oldUrl));
+  //console.log(api.url_shortener(oldUrl));
+});
+
 
 app.use(body_parser.json()); // support json encoded bodies
 app.use(body_parser.urlencoded({ extended: false })); // support encoded bodies
 app.post('/api/v1/url', function (req, res) {
-  var preUrl = "an.dy";
   var oldUrl = req.body.url; //Stores actual URL Input
-  var randomNum = Math.round(Math.random() * (9999 - 1000) + 1000); //randomizer
-  var newURL = preUrl+"/"+randomNum;
 
-  res.json({original_url: oldUrl, shortened_url: newURL});
+  res.json(api.url_shortener(oldUrl));
   //console.log(req.body.url);
 })
 
