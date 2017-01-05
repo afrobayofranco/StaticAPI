0d06fd1f7a4b3f1c836a9acd991f74535184216b
afrobayofranco <afrobayofranco@fullsail.com>
2017-01-04 20:04:32 -0500

    Addded comments

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
+//This is the functionality for the URL shortener. It Exports the shortened URL.
 exports.url_shortener = (oldUrl) => {
 
-    var preUrl = "an.dy";
+    //Store the parts of the shortened URL.
+    const preUrl = "an.dy"; //Prefix for the shortened Url
     var randomNum = Math.round(Math.random() * (9999 - 1000) + 1000); //randomizer
-    var newURL = preUrl+"/"+randomNum;
+    var newURL = preUrl+"/"+randomNum; //New Url including random number.
 
-    //New URL response
+    //New URL response, It includes the old Url
     return {original_url: oldUrl, shortened_url: newURL};
     //console.log(req.params.url);
 }
--- src/server.js
+++ src/server.js
@@ -1,24 +1,37 @@
+//constances required for the API to function
 const express = require('express')
 const body_parser = require('body-parser')
 const api = require('./api.js')
 const app = express()
 
-
+/*
+GET option, obtains the url to be shortened through the address line.
+Endpoint is /api/v1/:url
+*/
 app.get('/api/v1/:url', (req, res) => {
-  var oldUrl = req.params.url; //Stores actual URL Input
+  //Stores the URL Input
+  var oldUrl = req.params.url; //Stores the URL Input
 
+  //response in json file. The argument is the URL Input.
   res.json(api.url_shortener(oldUrl));
   //console.log(api.url_shortener(oldUrl));
 });
 
 
+/*
+POST option, obtains the url to be shortened through the body
+Endpoint is /api/v1/url
+*/
 app.use(body_parser.json()); // support json encoded bodies
 app.use(body_parser.urlencoded({ extended: false })); // support encoded bodies
 app.post('/api/v1/url', function (req, res) {
-  var oldUrl = req.body.url; //Stores actual URL Input
+  //Stores the URL Input
+  var oldUrl = req.body.url;
 
+  //response in json file. The argument is the URL Input.
   res.json(api.url_shortener(oldUrl));
   //console.log(req.body.url);
 })
 
+//Server listens in port:3000
 app.listen(3000)
