8a49e5f3bbacb8f4c418486d0a2ccafd5fe454d9
afrobayofranco <afrobayofranco@fullsail.com>
2017-01-06 17:52:10 -0500

    Sequelize and mysql installed and connected

---
+++ models/dbConnection.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('url_shortener', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
