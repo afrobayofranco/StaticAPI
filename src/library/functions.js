68813854dc147b68ee873637793edd01702310fb
afrobayofranco <afrobayofranco@fullsail.com>
2017-01-14 18:53:31 -0500

    Linting is done

--- src/library/functions.js
+++ src/library/functions.js
@@ -1,9 +1,8 @@
 // This is the random number generator, returns the randomNum to the url_shortener.
 function randomizer() {
   // Math function to generate random number between 1000000 and 9999999.
-  const randomNum = Math.round(Math.random() * (9999999 - 1000000) + 1000000);
+  return Math.round(Math.random() * ((9999999 - 1000000) + 1000000));
 }
-
 // This is the functionality for the URL shortener. It Exports the shortened URL.
 function urlShortener(oldUrl, customUrl) {
   // Store the parts of the shortened URL.
@@ -19,6 +18,5 @@ function urlShortener(oldUrl, customUrl) {
     shortened_url: newURL,
   };
 }
-
 // This exports the shortened_url: an.dy/XXXXXXX
 exports.url_shortener = urlShortener;
